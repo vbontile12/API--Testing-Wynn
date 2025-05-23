4b3a3b4b6afcabcf44af735c91a9177589b004da
Vuyani <vbontile@gmail.com>
2025-05-23 23:42:20 +0200

    added read me file

---
+++ README.m
@@ -0,0 +1,12 @@
+# Playwright API Tests 
+
+This project automates file uploads using **Playwright**.
+
+## Setup Instructions
+
+```bash
+git clone https://github.com/vbontile12/API--Testing-Wynn.git
+cd API Testing-Wynn
+npm install
+npx playwright install
+npm test
